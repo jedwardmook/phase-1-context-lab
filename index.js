b08ada6c179e9d5e182848cc8df29dcdb5114255
John Mook <jedwardmook@yahoo.com>
2022-02-04 12:09:23 -0600

    commit tree f6a7ad288f1db8270f9b606cc02a8fe63aa19acc

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 /* Your Code Here */
+function createEmployeeRecord(employee){
+ 
+    return {  
+            firstName: employee[0],
+            familyName: employee[1],
+            title: employee[2],
+            payPerHour: employee[3],
+            timeInEvents : [],
+            timeOutEvents : [],
+        }
+}
+
+function createEmployeeRecords(array){
+    return array.map(array => createEmployeeRecord(array))
+}
+
+function createTimeInEvent(timeEvent){
+let [date, hour] = timeEvent.split(' ')
+let eventObj = {
+	type: 'TimeIn',
+	hour: parseInt(hour, 10),
+	date: date,
+}
+this.timeInEvents.push(eventObj)
+return this
+}
+
+function createTimeOutEvent(timeEvent){
+let [date, hour] = timeEvent.split(' ')
+let eventObj = {
+    type: 'TimeOut',
+    hour: parseInt(hour, 10),
+    date: date,
+}
+this.timeOutEvents.push(eventObj)
+return this
+}
+
+function hoursWorkedOnDate(date){
+const timeIn = this.timeInEvents.find(event =>event.date === date)
+const timeOut = this.timeOutEvents.find(event =>event.date === date)
+
+return (timeOut.hour - timeIn.hour)/100
+}
+
+function wagesEarnedOnDate(date){
+//console.log(this)
+let hours = hoursWorkedOnDate.call(this, date)
+const wagesEarned = this.payPerHour * hours
+return wagesEarned
+}
+
+function calculatePayroll(employeeRecords){
+const record = employeeRecords.map(employee => allWagesFor.call(employee))
+
+return record.reduce((currentValue, total) => currentValue + total)
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +78,6 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(employees, firstNameString) {
+    return employees.find(emp => emp.firstName === firstNameString)
+}
